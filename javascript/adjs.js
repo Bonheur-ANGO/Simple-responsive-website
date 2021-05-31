948976a818f998fa8e881b8b9dc84328fb59ae92
Bonheur ANGO <angomillenium@gmail.com>
2021-05-31 19:33:14 +0200

    My initial commit

---
+++ javascript/adjs.js
@@ -0,0 +1,8 @@
+let span = document.querySelector('span');
+span.addEventListener('click', function(){
+    let nav = document.querySelector('.nav');
+    nav.classList.toggle('display-navbar');
+
+    /*let ul = document.querySelector('ul');
+    ul.classList.toggle('display-ul');*/
+});
